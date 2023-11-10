82e866affb9412805e970d53e74eb0a8b25a6970
RUVNE <dian.galleon@gmail.com>
2023-11-11 03:11:56 +0800

    fix: revert next config env

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    GCP_PRIVATE_KEY: process.env.NEXT_PUBLIC_GCP_PRIVATE_KEY,
-    GCP_PROJECT_ID: process.env.NEXT_PUBLIC_GCP_PROJECT_ID,
-    GCP_SERVICE_ACCOUNT_EMAIL: process.env.NEXT_PUBLIC_GCP_SERVICE_ACCOUNT_EMAIL,
-  }
+    GCP_PRIVATE_KEY: process.env.GCP_PRIVATE_KEY,
+    GCP_PROJECT_ID: process.env.GCP_PROJECT_ID,
+    GCP_SERVICE_ACCOUNT_EMAIL: process.env.GCP_SERVICE_ACCOUNT_EMAIL,  }
 }
 
 module.exports = nextConfig
