b70aa53d7234cc2cd068308e14f6af0e0becc938
RUVNE <dian.galleon@gmail.com>
2023-11-11 02:59:22 +0800

    fix: env next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  env: {
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+    GCP_PROJECT_ID: process.env.GCP_PROJECT_ID,
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  }
 }
 
 module.exports = nextConfig
