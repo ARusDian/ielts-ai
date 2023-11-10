da3800706547bff1cb1bd9c155b95f7f9dd03b5c
RUVNE <dian.galleon@gmail.com>
2023-11-11 03:01:00 +0800

    fix: GCP credentials env import

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+    GCP_PRIVATE_KEY: process.env.GCP_PRIVATE_KEY,
     GCP_PROJECT_ID: process.env.GCP_PROJECT_ID,
     GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GCP_SERVICE_ACCOUNT_EMAIL: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   }
 }
--- src/pages/api/synthesize-single.ts
+++ src/pages/api/synthesize-single.ts
@@ -54,11 +54,11 @@ export default function handler(
 
 export const getGCPCredentials = () => {
     // for Vercel, use environment variables
-    return process.env.GOOGLE_PRIVATE_KEY
+    return process.env.GCP_PRIVATE_KEY
         ? {
             credentials: {
-                client_email: process.env.GCLOUD_SERVICE_ACCOUNT_EMAIL,
-                private_key: process.env.GOOGLE_PRIVATE_KEY,
+                client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+                private_key: process.env.GCP_PRIVATE_KEY,
             },
             projectId: process.env.GCP_PROJECT_ID,
         }
@@ -71,7 +71,6 @@ export const getGCPCredentials = () => {
 
 
 async function authenticateImplicitWithAdc() {
-    console.log(process.env.GOOGLE_PRIVATE_KEY)
     console.log(getGCPCredentials());
     // This snippet demonstrates how to list buckets.
     // NOTE: Replace the client created below with the client required for your application.
