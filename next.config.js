80b3248cce181b12b2dd64b3d4370740fc375a91
RUVNE <dian.galleon@gmail.com>
2024-10-07 15:10:36 +0800

    fix: randomly refresh page

--- next.config.js
+++ next.config.js
@@ -10,9 +10,9 @@ const nextConfig = {
   },
   onDemandEntries: {
     // period (in ms) where the server will keep pages in the buffer
-    maxInactiveAge: 10 * 1000,
+    maxInactiveAge: 86400 * 1000,
     // number of pages that should be kept simultaneously without being disposed
-    pagesBufferLength: 1,
+    pagesBufferLength: 100,
   },
 }
 
