4e29b88c317232aaa6ae9b70bc9ca9c0a3dfb5ea
RUVNE <dian.galleon@gmail.com>
2024-10-07 14:59:30 +0800

    fix: randomly refresh page

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
   reactStrictMode: true,
   env: {
     GCP_PRIVATE_KEY: process.env.GCP_PRIVATE_KEY,
     GCP_PROJECT_ID: process.env.GCP_PROJECT_ID,
     GCP_SERVICE_ACCOUNT_EMAIL: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+  },
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 10 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 1,
   },
 }
 
